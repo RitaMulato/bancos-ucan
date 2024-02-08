10c5b1c4c85b8ec43379de31cd784fc1a3ecc29a
RitaMulato <ritaamandio96@gmail.com>
2024-02-08 17:34:18 +0100

    "feat: swagger"

--- banco-engenharia-service/src/main/java/edu/ucan/sdp2/bancoengenhariaservice/controllers/AutenticacaoController.java
+++ banco-engenharia-service/src/main/java/edu/ucan/sdp2/bancoengenhariaservice/controllers/AutenticacaoController.java
@@ -24,21 +24,30 @@ import org.springframework.web.multipart.MultipartFile;
 public class AutenticacaoController {
 
     private final AutenticacaoService service;
-
+    @Operation(summary = "verificar Telemovel", description = "Rota utilizada para enviar um código de verificação no telemovel")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful"),
+            @ApiResponse(responseCode = "404", description = "número de telemovel não encontrado")
+    })
     @PostMapping(value = "/verificar_telemovel")
     public ResponseEntity<Resposta> verificarTelemovel(@RequestParam String phone){
         return  service.enviarCodigoVerificacao(phone);
     }
 
+    @Operation(summary = "Confirmar Codigo", description = "Rota utilizada para confirmar o código de verificação")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful"),
+            @ApiResponse(responseCode = "404", description = "Código não encontrado")
+    })
     @Secured({"ROLE_USER"})
     @PostMapping(value = "/confirmar-codigo")
     public ResponseEntity<Resposta> confirmarCodigo(@RequestBody TelefoneVerificacao dto){
         return  service.verificacaoCodigoTelefone(dto.getTelefone(), dto.getCodigo());
     }
-    @Operation(summary = "Obter Pessoa", description = "Obter detalhes de uma pessoa por ID")
+    @Operation(summary = "Registrar Cliente", description = "Registrar um novo cliente")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successful"),
-            @ApiResponse(responseCode = "404", description = "Pessoa não encontrada")
+            @ApiResponse(responseCode = "404", description = "Cliente não encontrado")
     })
     @PostMapping(value= "/registar", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
     public ResponseEntity<Resposta> register(
@@ -47,6 +56,12 @@ public class AutenticacaoController {
     ) {
         return service.autoRegistoCliente(dto.withFicheiro(ficheiro));
     }
+
+    @Operation(summary = "Login", description = "Rota utilizada para efectuar o login")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful"),
+            @ApiResponse(responseCode = "404", description = "Dados incorrectos")
+    })
     @PostMapping("/login")
     public ResponseEntity<Resposta> login(
             @RequestBody UtilizadorLoginRequisicaoDto dto
@@ -54,8 +69,13 @@ public class AutenticacaoController {
         return service.login(dto);
     }
 
+    @Operation(summary = "Alterar a palavra passe", description = "Rota utilizada para alterar a palavra passe")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful"),
+            @ApiResponse(responseCode = "404", description = "Dados incorrectos")
+    })
     @PostMapping(value = "/resetar-pwd")
-    public ResponseEntity<Resposta> resetPassword(@RequestBody ResetSenhaRequisicaoDto request){
+    public ResponseEntity<Resposta> alterarPassword(@RequestBody ResetSenhaRequisicaoDto request){
         return  service.passwordReset(request);
     }
 
